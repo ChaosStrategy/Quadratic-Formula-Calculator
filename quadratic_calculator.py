1b0fc2cf4c6661f9d1a32fdd5b88e90adefa96ac
ChaosStrategy <bangell24@falmouthacademy.org>
2021-09-08 10:39:59 -0400

    Added a b and c value variables through user input

--- quadratic_calculator.py
+++ quadratic_calculator.py
@@ -1,1 +1,9 @@
 import math
+import random
+import matplotlib
+
+## a b and c value variables through user input
+
+avalue = input("Please type value A:")
+bvalue = input("Please type value B:")
+cvalue = input('Please type value C:')
