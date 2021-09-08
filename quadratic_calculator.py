dc8bcba6d18ce62dc391e0b4fcc31bdf27269304
ChaosStrategy <bangell24@falmouthacademy.org>
2021-09-08 10:59:59 -0400

    matplotlib commented out

--- quadratic_calculator.py
+++ quadratic_calculator.py
@@ -15,14 +15,14 @@ print("your x intercepts are equal to ", xvalue1, " and ", xvalue2)
 
 
 ## Ploimport matplotlib.pyplot as plt
-import numpy as np
-import matplotlib.pyplot as plt
-
-# create 1000 equally spaced points between -10 and 10
-x = np.linspace(-10, 10, 1000)
-
-# calculate the y value for each element of the x vector
-y = avalue*x**2 + bvalue*x + cvalue
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# # create 1000 equally spaced points between -10 and 10
+# x = np.linspace(-10, 10, 1000)
+#
+# # calculate the y value for each element of the x vector
+# y = avalue*x**2 + bvalue*x + cvalue
+#
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
