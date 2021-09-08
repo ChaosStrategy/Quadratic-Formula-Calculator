6bcbafde2d381f5ddb1ff44c12b4e802754a38f9
ChaosStrategy <bangell24@falmouthacademy.org>
2021-09-08 10:44:21 -0400

    Added xvalue calculations

--- quadratic_calculator.py
+++ quadratic_calculator.py
@@ -7,3 +7,6 @@ import matplotlib
 avalue = input("Please type value A:")
 bvalue = input("Please type value B:")
 cvalue = input('Please type value C:')
+
+xvalue1 = (0-bvalue) + math.sqrt(((bvalue*bvalue)-(4*avalue*cvalue))/(2*avalue))
+xvalue2 = (0-bvalue) - math.sqrt(((bvalue*bvalue)-(4*avalue*cvalue))/(2*avalue))
